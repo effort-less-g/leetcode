90502f6cfd8712c62f6cfabd0b41e79c8e290d4f
effortless <36704039+effort-less-g@users.noreply.github.com>
2026-07-31 19:16:09 +0400

    Update 2542. Maximum Subsequence Score.py

--- LeetCode 75/Heap_Priority_Queue/2542. Maximum Subsequence Score.py
+++ LeetCode 75/Heap_Priority_Queue/2542. Maximum Subsequence Score.py
@@ -1,1 +1,24 @@
+from heapq import heappush, heappop
 
+class Solution:
+    def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
+        
+        heap = []
+        
+        sorted_tuple = list(zip(nums1, nums2))
+
+        sorted_tuple.sort(key=lambda x:-x[1])
+
+        # print(sorted_tuple)
+
+        res, sum = 0, 0
+
+        for a, b in sorted_tuple:
+            sum += a 
+            heappush(heap, a)
+            # print(heap)
+            if len(heap) == k:
+                res = max(res, sum*b)
+                sum -= heappop(heap)
+
+        return res
