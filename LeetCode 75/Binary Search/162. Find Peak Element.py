d185fe6ae6c44728b97a43860ff2200824687b0c
effortless <36704039+effort-less-g@users.noreply.github.com>
2026-08-03 23:17:55 +0400

    Update 162. Find Peak Element.py

--- LeetCode 75/Binary Search/162. Find Peak Element.py
+++ LeetCode 75/Binary Search/162. Find Peak Element.py
@@ -1,1 +1,35 @@
+class Solution:
 
+    def rec(self, nums, left, right):
+
+        # print(left, right)
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid-1] < nums[mid] > nums[mid + 1]:
+                return mid
+            else:
+                ans = self.rec(nums, mid+1, right)
+                if ans:
+                    return ans
+                else:
+                    return self.rec(nums, left, mid-1)
+
+    def findPeakElement(self, nums: List[int]) -> int:
+        left = 0 
+        right = len(nums) - 1
+
+        if len(nums) == 1:
+            return 0
+        elif nums[0] > nums[1]:
+            return 0
+        elif nums[-1] > nums[-2]:
+            return len(nums) - 1
+        else:
+            res = self.rec(nums, left, right)
+
+            if res:
+                return res
+            else:
+                return nums.index(max(nums))
