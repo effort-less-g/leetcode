f059b4ad762bd24a9efd722be7f595c4435e99e6
effortless <36704039+effort-less-g@users.noreply.github.com>
2026-08-04 07:51:10 +0400

    Add letter combinations solution using backtracking
    Implement backtracking to generate letter combinations for a phone number.

---
+++ LeetCode 75/Backtracking/17 Letter Combinations of a Phone Number.py
@@ -0,0 +1,29 @@
+class Solution:
+
+    combos = {
+        '2': ['a', 'b', 'c'],
+        '3': ['d', 'e', 'f'],
+        '4': ['g', 'h', 'i'],
+        '5': ['j', 'k', 'l'],
+        '6': ['m', 'n', 'o'],
+        '7': ['p', 'q', 'r', 's'],
+        '8': ['t', 'u', 'v'],
+        '9': ['w', 'x', 'y', 'z']
+    }
+
+    def bt_dfs(self, path, length):
+
+        if len(path) == length:
+            return path
+
+        for digit in digits:
+            for choice in combos[digit]:
+                path += choice
+                return self.bt_dfs(path, len(path))
+                path.pop()
+
+    def letterCombinations(self, digits: str) -> List[str]:
+
+        path = ""
+        self.bt_dfs(path, len(path))
+
