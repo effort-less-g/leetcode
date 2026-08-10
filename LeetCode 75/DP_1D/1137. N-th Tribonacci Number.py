c4aee7943c6388e49d3f3967b36395832eeb7971
effortless <36704039+effort-less-g@users.noreply.github.com>
2026-08-10 23:38:02 +0400

    Update 1137. N-th Tribonacci Number.py

--- LeetCode 75/DP_1D/1137. N-th Tribonacci Number.py
+++ LeetCode 75/DP_1D/1137. N-th Tribonacci Number.py
@@ -1,1 +1,15 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
 
+        tribonachi = [0, 1, 1]
+
+        if n < 3:
+            return tribonachi[n]
+
+        for i in range(3, n+1):
+            a = tribonachi[i-3]
+            b = tribonachi[i-2]
+            c = tribonachi[i-1]
+            tribonachi.append(a+b+c)
+
+        return tribonachi[-1]
